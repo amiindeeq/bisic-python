8efa07c008fc2d6806750b175e970009b7425e96
amiindeeq <muhiimdajiya@gmail.com>
2023-09-13 14:48:34 +0300

    add .

---
+++ calcute.py
@@ -0,0 +1,48 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Cannot divide by zero"
+
+print("Welcome to the calculator!")
+
+while True:
+    print("Select an operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == '5':
+        print("Goodbye!")
+        break
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print("Result:", result)
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print("Result:", result)
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print("Result:", result)
+    elif choice == '4':
+        result = divide(num1, num2)
+        print("Result:", result)
+    else:
+        print("Invalid choice. Please try again.")
---
+++ index.py
@@ -0,0 +1,12 @@
+#for i in range(10,20):
+   # print("hello from vscode")
+
+ibraahim = 5
+axmed = "hello"
+f = 3.5
+a = 5.5
+b = a * f
+print(b)
+print(type(axmed))
+print(ibraahim,axmed,f)
+print("the value in ibraahin",ibraahim)
